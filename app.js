11f1e96faa79a031741eff1eb6e921968577e89e
Lily <ling19971930@gmail.com>
2023-02-08 11:38:53 +0800

    -feat: model restaurant add isDone params

--- app.js
+++ app.js
@@ -1,25 +1,105 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const Restaurant = require("./models/Restaurant")
+const bodyParser = require('body-parser')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const app = express()
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: '.hbs' }))
+app.set("view engine", "hbs")
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+// 取得資料庫連線狀態
 const db = mongoose.connection
-db.on('error', ()=>{
-  console.log('mongodb error')
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
 })
 
-db.once('open', ()=>{
-  console.log('mongodb connected')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.log(error))
+
+})
+app.get("/restaurants/new", (req, res) => {
+  res.render("new")
 })
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.post('/restaurants', (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.create({ id, name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
-app.listen(3000, ()=>{
-  console.log(`express is listening on http://localhost:3000`)
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+
+})
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+
+  const { fakeId, name, name_en, category, image, location, phone, google_map, rating, description, isDone } = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name,
+        restaurant.name_en = name_en,
+        restaurant.category = category,
+        restaurant.image = image,
+        restaurant.location = location,
+        restaurant.phone = phone,
+        restaurant.google_map = google_map,
+        restaurant.rating = rating,
+        restaurant.description = description
+        restaurant.isDone = isDone === 'on'
+      restaurant.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.listen(3000, console.log('App is listening on http://localhost:3000'))
